cdd39759e027f3be987d70adab47c8d2a6061686
sayder2020 <910660281@qq.com>
2026-05-04 10:16:49 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,8 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
+export default defineConfig({
+  plugins: [react()],
+  // 确保打包后资源路径正确，防止部署后找不到 JS/CSS
+  base: './', 
+})
